7d68d4ae8497f847718642ab4341760870b2e581
icondev99 <@icondev99>
2022-03-09 13:41:15 +0545

    Azure config added.

--- truffle-config.js
+++ truffle-config.js
@@ -66,6 +66,11 @@ module.exports = {
             timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         },
+        azure: {
+            host: "http://20.25.84.147", // Localhost (default: none)
+            port: 8545, // Standard Ethereum port (default: none)
+            network_id: "*", // Any network (default: none)
+        },
         // Useful for private networks
         // private: {
         // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
