1388d9ac0b21d5ecd8ab44b61eb0a31d3253fec5
icondev99 <@icondev99>
2022-03-09 22:05:38 +0545

    Latest contract address info.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
 const Vaccination = artifacts.require("Vaccination");
 
-module.exports = function (deployer, network, accounts) {
-    deployer.deploy(Vaccination);
-};
+module.exports = async function (deployer, network, accounts) {
+    await deployer.deploy(Vaccination);
+    console.log(typeof(network))
+    fs.appendFileSync('./contractAddress', `\n${network}: ${Vaccination.address}`);
+}
