d8bb6925838b937b492998b13ba114bdb2414ad3
icondev99 <@icondev99>
2022-03-09 13:44:28 +0545

    Azure added as private network

--- truffle-config.js
+++ truffle-config.js
@@ -66,17 +66,13 @@ module.exports = {
             timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         },
-        azure: {
-            host: "http://20.25.84.147", // Localhost (default: none)
-            port: 8545, // Standard Ethereum port (default: none)
-            network_id: "*", // Any network (default: none)
-        },
+
         // Useful for private networks
-        // private: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-        // network_id: 2111,   // This network is yours, in the cloud.
-        // production: true    // Treats this network as if it was a public net. (default: false)
-        // }
+        azure: {
+            provider: () => new HDWalletProvider(mnemonic, `http://20.25.84.147:8545`),
+            network_id: "*",   // This network is yours, in the cloud.
+            // production: true    // Treats this network as if it was a public net. (default: false)
+        }
     },
 
     // Set default mocha options here, use special reporters etc.
